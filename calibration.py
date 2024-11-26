84fbd49b743d2345e225f80361885d5b0c545211
IsabelMorell <202207975@alu.comillas.edu>
2024-11-26 18:28:02 +0100

    primera sesion

---
+++ calibration.py
@@ -0,0 +1,16 @@
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import imageio
+import cv2
+import copy
+import glob
+import os
+
+def find_chessboard_corners(imgs: List[np.array()], dimensions: Tuple[int,int]):
+    corners = [cv2.findChessboardCorners(img, (8, 6), None) for img in imgs]
+    return corners
+
+
--- test.py
+++ test.py
@@ -1,20 +1,36 @@
 import cv2
 from picamera2 import Picamera2
+import os
+import time
 
-def stream_video():
+def stream_video(folder_path, threshold, duration):
     picam = Picamera2()
     picam.preview_configuration.main.size=(1280, 720)
     picam.preview_configuration.main.format="RGB888"
     picam.preview_configuration.align()
     picam.configure("preview")
     picam.start()
-
-    while True:
+    
+    contador = 0
+    tiempo_actual = time.time()
+    while (time.time() - tiempo_actual) <= duration:
         frame = picam.capture_array()
-        cv2.imshow("picam", frame)
+        
+        if time.time() - tiempo_actual >= threshold:
+            if contador < 10:
+                frame_name = f"chessboard_0{contador}.jpg"
+            else:
+                frame_name = f"chessboard_{contador}.jpg"
+            cv2.imshow("picam", frame)
+            cv2.save_images(frame, frame_name, folder_path)
+            contador += 1
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    stream_video()
+    threshold = 1
+    duration = 30
+    folder_path = os.path.join(os.path.dirname(os.getcwd()), "data", "chessboard_frames")
+    stream_video(folder_path, threshold, duration)
